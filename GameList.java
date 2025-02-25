92793c16347966d4722d0de7631be03c26ef004f
Eclipse-V <christinevanson77@gmail.com>
2025-02-25 13:18:18 -0500

    Update GameList.java

--- GameList.java
+++ GameList.java
@@ -105,7 +105,7 @@ public class GameList{
             Game currentGame = superBowls.get(i);
             String minScoreDiff = currentGame.getScore();
             //getting the score differnce 
-            int dash1 = minScoreDiff.indexOf("–");
+            int dash1 = minScoreDiff.indexOf("\–");
             int firstScore1 = Integer.valueOf(minScoreDiff.substring(0, dash1));
             int secondScore1 = Integer.valueOf(minScoreDiff.substring(dash1 + 1));
             int diff1 = Math.abs((firstScore1 - secondScore1));
@@ -113,7 +113,7 @@ public class GameList{
             for(int j = i + 1; j < superBowls.size(); j++){
                 Game nextGame = superBowls.get(j);
                 String nextScoreDiff = nextGame.getScore();
-                int dash2 = minScoreDiff.indexOf("–");
+                int dash2 = minScoreDiff.indexOf("\–");
                 int firstScore2 = Integer.valueOf(nextScoreDiff.substring(0, dash1));
                 int secondScore2 = Integer.valueOf(nextScoreDiff.substring(dash1 + 1));
                 int diff2 = Math.abs((firstScore2 - secondScore2));
